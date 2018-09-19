acec915598f394578edc47c36de79ee6a070d47d
Kevin Ednalino <kcednalino@gmail.com>
2018-09-19 19:32:49 +0100

    Refactored function init-texture into parse-glyphs-ppm

--- src/view/raster.lisp
+++ src/view/raster.lisp
@@ -33,7 +33,7 @@
 
     (gl:use-program program-raster)
     
-    ;; Init buffer texture will upload msdf glyphs also
+    ;; Init buffer object
     (gl:active-texture :texture0)
     (setf bo-texture (init-buffer-texture program-raster
     					  :texture-buffer
@@ -45,6 +45,8 @@
     					  t
     					  :buffering 'single)) ;triple if editing? rotate also...
     (%gl:uniform-1i (gl:get-uniform-location program-raster "msdf") 0)
+    ;; Parse glyph images into texture
+    (parse-glyphs-ppm bo-texture)
 
     
     (setf boav-main (init-boav-main))
@@ -134,3 +136,60 @@
 			      0) ;; nullptr or offset into buffer
   
   t)
+
+
+(defun read-ppm (filepath)
+  (let ((bitmap (make-array (* 96 96 4)
+			    :fill-pointer 0
+			    :element-type '(unsigned-byte 8))))
+    (with-open-file (stream filepath)
+
+      ;; Skip first 3 lines
+      (dotimes (i 3)
+	(read-line stream nil))
+      
+      ;; OpenGL textures are row-major order = row/column,  e.g.3x2:
+      ;; -> -> -> \n
+      ;; -> -> ->
+      ;; Line 0: (0,0)
+      ;; Line 1: (1,0)
+      ;; Line 2: (2,0)
+      ;; Line 3: (0,1)
+      ;; Line 4: (1,1)
+      ;; Line 5: (2,1)
+      ;; Note, however, OpenGL matrices are column-major order,
+      ;; somewhat by historical accident
+      
+      ;; Each line has 12 space delimited values
+      ;; 12 / 3 components per pixel = 4 pixels every line
+
+      (loop 
+         :for pixels := (read-line stream nil)
+	 :while pixels
+	 :do (loop :for c :in (str:split-omit-nulls " " pixels)
+		:with i = 0
+		:do (progn
+		      (vector-push (parse-integer c) bitmap)
+		      (incf i)
+		      (when (= i 3)
+			(vector-push 255 bitmap)
+			(setf i 0))))))
+    bitmap))
+
+(defun parse-glyphs-ppm (buffer)
+  (loop
+     :for code :from 32 :to 255
+     :with msdf-glyphs-path := (merge-pathnames #P"glyphs-msdf/" (asdf:system-source-directory :protoform))
+     :for ppm-path := (merge-pathnames (make-pathname :name (write-to-string code) :type "ppm")
+				       msdf-glyphs-path)
+     :with i := 0
+     :with ptr := (aref (ptrs-buffer buffer) 0)
+     :do (let* ((bmp (read-ppm ppm-path)))
+	   
+    	   (assert (= (length bmp) (* 96 96 4)))
+
+	   (loop 
+	      :for c :across bmp
+	      :do (progn
+		    (setf (mem-aref ptr :unsigned-char i) c)
+		    (incf i))))))  
