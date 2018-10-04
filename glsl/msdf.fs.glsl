9cfd214e0b350d496370657e039e7adf71e94624
Kevin Ednalino <kcednalino@gmail.com>
2018-10-04 17:08:13 +0100

    Cleaned up fragment shader

--- glsl/msdf.fs.glsl
+++ glsl/msdf.fs.glsl
@@ -20,18 +20,18 @@ vec4 filter_bilinear()
 {
     // https://github.com/WebGLSamples/WebGL2Samples/blob/master/samples/texture_fetch.html
     
-    vec2 f_dims = vec2(float(vertexDimsTex.x-1), float(vertexDimsTex.y-1));
+    // Move this to vertex shader or CPU/passthrough
+    vec2 f_dims = vec2(float(vertexDimsTex.x-0), float(vertexDimsTex.y-0));
     vec2 f_coordTexel = vec2(vertexUV.u, vertexUV.v) * f_dims;
     ivec2 coordTexel = ivec2(f_coordTexel);
         
-    // offset is based on previous
+    // Below only works when textures are all the same size
     //int offsetTex = vertexOffsetTex * vertexDimsTex.x * vertexDimsTex.y;
-    int offsetTex = vertexOffsetTex;
         
-    int offsetTexel00 = offsetTex + (coordTexel.x + (coordTexel.y * vertexDimsTex.x));
-    int offsetTexel10 = offsetTex + ((coordTexel.x + 1) + (coordTexel.y * vertexDimsTex.x));
-    int offsetTexel11 = offsetTex + ((coordTexel.x + 1) + ((coordTexel.y + 1) * vertexDimsTex.x));
-    int offsetTexel01 = offsetTex + (coordTexel.x + ((coordTexel.y + 1) * vertexDimsTex.x));
+    int offsetTexel00 = vertexOffsetTex + (coordTexel.x + (coordTexel.y * vertexDimsTex.x));
+    int offsetTexel10 = vertexOffsetTex + ((coordTexel.x + 1) + (coordTexel.y * vertexDimsTex.x));
+    int offsetTexel11 = vertexOffsetTex + ((coordTexel.x + 1) + ((coordTexel.y + 1) * vertexDimsTex.x));
+    int offsetTexel01 = vertexOffsetTex + (coordTexel.x + ((coordTexel.y + 1) * vertexDimsTex.x));
     
     vec4 texel00 = texelFetch(msdf, offsetTexel00);
     vec4 texel10 = texelFetch(msdf, offsetTexel10);
@@ -45,25 +45,6 @@ vec4 filter_bilinear()
     return mix(texel0, texel1, sampleCoord.x);
 }    
 
-/*
-//uniform sampler2DArray msdf;
-vec4 filter_bilinear_2d_array()
-{
-    // https://github.com/WebGLSamples/WebGL2Samples/blob/master/samples/texture_fetch.html
-    vec2 size = vec2(textureSize(msdf, 0) - 1);
-    vec2 texcoord = vec2(vertexUV.u, vertexUV.v) * size;
-    ivec2 coord = ivec2(texcoord);
-    vec4 texel00 = texelFetch(msdf, ivec3(coord + ivec2(0, 0), vertexW_UV), 0);
-    vec4 texel10 = texelFetch(msdf, ivec3(coord + ivec2(1, 0), vertexW_UV), 0);
-    vec4 texel11 = texelFetch(msdf, ivec3(coord + ivec2(1, 1), vertexW_UV), 0);
-    vec4 texel01 = texelFetch(msdf, ivec3(coord + ivec2(0, 1), vertexW_UV), 0);
-    vec2 sampleCoord = fract(texcoord.xy);
-    vec4 texel0 = mix(texel00, texel01, sampleCoord.y);
-    vec4 texel1 = mix(texel10, texel11, sampleCoord.y);            
-    return mix(texel0, texel1, sampleCoord.x);
-}    
-*/
-
 void main()
 {
     // Note UV's v coord flipped in vertex shader
@@ -76,6 +57,6 @@ void main()
     //color = texelFetch(msdf, coord.x + (coord.y * 58));
     
     // Switch B and R due to Pango layout
-    vec4 color_filtered = filter_bilinear();
-    color = vec4(color_filtered.b, color_filtered.g, color_filtered.r, color_filtered.a);
+    vec4 colorFiltered = filter_bilinear();
+    color = vec4(colorFiltered.b, colorFiltered.g, colorFiltered.r, colorFiltered.a);
 }
