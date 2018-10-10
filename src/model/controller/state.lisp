a9b7f37de2cca8384195942363a5fcb934489070
Kevin Ednalino <kcednalino@gmail.com>
2018-10-10 19:12:59 +0100

    Fixed indentation

--- src/model/controller/state.lisp
+++ src/model/controller/state.lisp
@@ -73,55 +73,55 @@
 	       key-states
 	       key-callbacks)
       *controller*
-        
-      (let* ((keyboard-event (libinput:event-get-keyboard-event event))
-	     (ev-state (libinput:event-keyboard-get-key-state keyboard-event))
-	     (ev-keycode (libinput:event-keyboard-get-key keyboard-event)))
-	;; event-keyboard-get-time
-	;; event-keyboard-get-time-usec
+    
+    (let* ((keyboard-event (libinput:event-get-keyboard-event event))
+	   (ev-state (libinput:event-keyboard-get-key-state keyboard-event))
+	   (ev-keycode (libinput:event-keyboard-get-key keyboard-event)))
+      ;; event-keyboard-get-time
+      ;; event-keyboard-get-time-usec
 
-	(with-slots (context keymap state repeat-char repeat-timer repeat-key repeat-delay repeat-interval
-			     mods-depressed mods-latched mods-locked)
-	    xkb
+      (with-slots (context keymap state repeat-char repeat-timer repeat-key repeat-delay repeat-interval
+			   mods-depressed mods-latched mods-locked)
+	  xkb
 	
-	  (let* ((keysym (xkb:xkb-state-key-get-one-sym state (+ ev-keycode 8)))
-		 (repeats (if (= (xkb:xkb-keymap-key-repeats keymap (+ ev-keycode 8)) 1)
-			      t
-			      nil))
-		 (keysym-char (xkb:get-key-character state (+ ev-keycode 8))))
-	    ;; (keysym-char2 (xkb:get-keysym-character keysym)))
-	    ;; (keysym-name (xkb:get-keysym-name keysym)))
-	    
-	    (xkb:xkb-state-update-key state
-				      (+ ev-keycode 8)
-				      (if (= ev-state +state-event-press+)
-					  +state-event-press+
-					  +state-event-release+))
+	(let* ((keysym (xkb:xkb-state-key-get-one-sym state (+ ev-keycode 8)))
+	       (repeats (if (= (xkb:xkb-keymap-key-repeats keymap (+ ev-keycode 8)) 1)
+			    t
+			    nil))
+	       (keysym-char (xkb:get-key-character state (+ ev-keycode 8))))
+	  ;; (keysym-char2 (xkb:get-keysym-character keysym)))
+	  ;; (keysym-name (xkb:get-keysym-name keysym)))
+	  
+	  (xkb:xkb-state-update-key state
+				    (+ ev-keycode 8)
+				    (if (= ev-state +state-event-press+)
+					+state-event-press+
+					+state-event-release+))
 
-	    (setf mods-depressed (xkb:xkb-state-serialize-mods state 1))
-	    (setf mods-latched (xkb:xkb-state-serialize-mods state 2))
-	    (setf mods-locked (xkb:xkb-state-serialize-mods state 4))
-	    (setf mods-group (xkb:xkb-state-serialize-mods state 8))
-	    ;; (format t "~a, ~a, ~a~%" mods-depressed mods-latched mods-locked)
+	  (setf mods-depressed (xkb:xkb-state-serialize-mods state 1))
+	  (setf mods-latched (xkb:xkb-state-serialize-mods state 2))
+	  (setf mods-locked (xkb:xkb-state-serialize-mods state 4))
+	  (setf mods-group (xkb:xkb-state-serialize-mods state 8))
+	  ;; (format t "~a, ~a, ~a~%" mods-depressed mods-latched mods-locked)
 
-	    (when (= ev-state +state-event-press+)
-	      (fmt-model t "update-keybaord"
-			 "code: ~a, sym: ~a, state: ~a, rep: ~a~%"
-			 (+ ev-keycode 8) (code-char keysym) ev-state repeats))
+	  (when (= ev-state +state-event-press+)
+	    (fmt-model t "update-keybaord"
+		       "code: ~a, sym: ~a, state: ~a, rep: ~a~%"
+		       (+ ev-keycode 8) (code-char keysym) ev-state repeats))
 
-	    ;; Update key state
-	    ;; If key is repeated, state will be set to 'repeat at a later time
-	    (setf (gethash keysym key-states)
-		  (if (= ev-state +state-event-press+)
-		      'press
-		      'release))
+	  ;; Update key state
+	  ;; If key is repeated, state will be set to 'repeat at a later time
+	  (setf (gethash keysym key-states)
+		(if (= ev-state +state-event-press+)
+		    'press
+		    'release))
 
-	    ;; Check timer for repeatable keys
-	    (when repeats
-	      (update-repeat-timer xkb
-				   ev-state
-				   keysym
-				   keysym-char)))))))
+	  ;; Check timer for repeatable keys
+	  (when repeats
+	    (update-repeat-timer xkb
+				 ev-state
+				 keysym
+				 keysym-char)))))))
 
 (defun reset-keys (key-states)
   ;; Any keys that were set to 'release in previous frame -> set to 'up
