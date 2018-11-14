5a3b9a939b0c973315dacee3b82b67f4670e7a21
Kevin Ednalino <kcednalino@gmail.com>
2018-11-14 17:20:08 +0000

    Cleaned up operators spacing

--- src/model/node/operators.lisp
+++ src/model/node/operators.lisp
@@ -3,30 +3,30 @@
 (defun displace-node-x (node
 			displacement
 			type-displace)
-    (with-slots (model-matrix
-		 index)
-	node
-      (cond ((eq type-displace :abs)
-	     (setf (vx3 (translation model-matrix)) displacement))
-	    ((eq type-displace :rel)
-	     (incf (vx3 (translation model-matrix)) displacement))
-	    (t
-	     (error "Unknown type-displace")))
-      (update-transform model-matrix)))
+  (with-slots (model-matrix
+	       index)
+      node
+    (cond ((eq type-displace :abs)
+	   (setf (vx3 (translation model-matrix)) displacement))
+	  ((eq type-displace :rel)
+	   (incf (vx3 (translation model-matrix)) displacement))
+	  (t
+	   (error "Unknown type-displace")))
+    (update-transform model-matrix)))
 
 (defun displace-node-y (node
 			displacement
 			type-displace)
-    (with-slots (model-matrix
-		 index)
-	node
-      (cond ((eq type-displace :abs)
-	     (setf (vy3 (translation model-matrix)) displacement))
-	    ((eq type-displace :rel)
-	     (incf (vy3 (translation model-matrix)) displacement))
-	    (t
-	     (error "Unknown type-displace")))
-      (update-transform model-matrix)))
+  (with-slots (model-matrix
+	       index)
+      node
+    (cond ((eq type-displace :abs)
+	   (setf (vy3 (translation model-matrix)) displacement))
+	  ((eq type-displace :rel)
+	   (incf (vy3 (translation model-matrix)) displacement))
+	  (t
+	   (error "Unknown type-displace")))
+    (update-transform model-matrix)))
 
 (defun eval-node-msdf (seq-key)
   ;; To eval, build up string from predecessors
