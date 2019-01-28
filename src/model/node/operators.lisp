50ead333d71d7c6563b5f0a9d02414e26e9dcac7
Kevin Ednalino <kcednalino@gmail.com>
2019-01-28 17:54:26 +0000

    Moved cut/copy/paste functions around in operators

--- src/model/node/operators.lisp
+++ src/model/node/operators.lisp
@@ -133,6 +133,63 @@
 	       (setf (data node) output-eval)
 	       t)))))
 
+;; (defun link-nodes (node-a node-b)
+;;   ;; Need not pos argument - user switches args
+;;   (insert-edge node-a node-b))
+
+;; (defun unlink-nodes (node-a node-b)
+;;   (remove-edge node-a node-b))
+
+(defun swap-nodes (node-src node-dest)
+  ;; Swap links
+  ;; - option to swap positions?
+  
+  ;; Get preds of src
+  ;; Remove edges
+  ;; Get preds of dest
+  ;; Remove edges
+  ;; Insert edges between src pres and dest
+  ;; Swap positions
+  t)
+
+(defun swap-id-with-node (node-src node-dest)
+  ;; node-src should be chars or string object
+  ;; node-dest should be dest
+  ;;
+  ;; provide inverse operation to produce id from node
+  t)
+
+(defun show-node-ids ()
+  ;; Procedure:
+  t)
+
+
+;; Pass starting position, i.e. node-pointer position
+;; For nodes, index to calculate
+;; Have to set index serially...
+;; - faster way would be to use spatial relationship to calculate index
+;;   -> only works when nodes are sequentially positioned
+;;   -> radial would be better
+;;
+
+;; pos = start + (advance * index)
+;; adjust-pos by bounds
+
+;; 	(digraph:mapc-vertices (lambda (vert)
+;; 				 (sb-concurrency:enqueue
+;; 				  (list nil
+;; 					(make-symbol (format nil "node-~a~%" (index vert)))
+;; 					'()
+;; 					(lambda ()
+;; 					  (format t "~S~%" (translation (model-matrix vert)))
+;; 					  ;; (funcall #'randomize-color-node vert)
+;; 					  ))
+;; 				  *queue-anim*))
+;; 			       *digraph*))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; (format t "*:: ~S~%" (digraph:predecessors *digraph* *node-pointer*))
 ;; (format t "*:: ~S~%" (digraph:successors *digraph* *node-pointer*))
 
@@ -391,57 +448,3 @@
 	(enqueue-node node-buf))
       (enqueue-node node-ref)
       (enqueue-node-pointer))))
-
-;; (defun link-nodes (node-a node-b)
-;;   ;; Need not pos argument - user switches args
-;;   (insert-edge node-a node-b))
-
-;; (defun unlink-nodes (node-a node-b)
-;;   (remove-edge node-a node-b))
-
-(defun swap-nodes (node-src node-dest)
-  ;; Swap links
-  ;; - option to swap positions?
-  
-  ;; Get preds of src
-  ;; Remove edges
-  ;; Get preds of dest
-  ;; Remove edges
-  ;; Insert edges between src pres and dest
-  ;; Swap positions
-  t)
-
-(defun swap-id-with-node (node-src node-dest)
-  ;; node-src should be chars or string object
-  ;; node-dest should be dest
-  ;;
-  ;; provide inverse operation to produce id from node
-  t)
-
-(defun show-node-ids ()
-  ;; Procedure:
-  t)
-
-
-;; Pass starting position, i.e. node-pointer position
-;; For nodes, index to calculate
-;; Have to set index serially...
-;; - faster way would be to use spatial relationship to calculate index
-;;   -> only works when nodes are sequentially positioned
-;;   -> radial would be better
-;;
-
-;; pos = start + (advance * index)
-;; adjust-pos by bounds
-
-;; 	(digraph:mapc-vertices (lambda (vert)
-;; 				 (sb-concurrency:enqueue
-;; 				  (list nil
-;; 					(make-symbol (format nil "node-~a~%" (index vert)))
-;; 					'()
-;; 					(lambda ()
-;; 					  (format t "~S~%" (translation (model-matrix vert)))
-;; 					  ;; (funcall #'randomize-color-node vert)
-;; 					  ))
-;; 				  *queue-anim*))
-;; 			       *digraph*))
