9b1161fad6816950e5c67b4d275922a0e200b6c4
Kevin Ednalino <kcednalino@gmail.com>
2019-02-01 17:07:50 +0000

    Refactored pop-node and delete-node; return type and moved translation functionality to delete-node

--- src/model/node/primary.lisp
+++ src/model/node/primary.lisp
@@ -226,40 +226,6 @@
 ;;;;;;;;;;;;;
 ;; relational
 
-(defun add-node (data &optional (move-pointer t)) ; rename add-node-to-ptr
-  (let* ((baseline (get-origin-from-node-pos *node-pointer*))
-	 (node (init-node-msdf baseline
-			       *scale-node*
-			       (pop *stack-i-nodes*)
-			       data)))
-    
-    (insert-vertex node)
-    (spatial-trees:insert node *r-tree*)
-    
-    ;; Factor this out...
-    (when-let* ((node-ptr-out (get-node-ptr-out))
-		(node-type (get-node-type node-ptr-out)))
-
-	       ;; Push new node above for now
-	       (when (or (eq node-type :intra)
-			 (eq node-type :start))
-		 (let* ((bounds-origin (bounds-origin (gethash (char-code (data node)) *metrics*))))
-		   (translate-node-to-node node
-					   node
-					   :offset (vec3 0.0
-    							 (* +linegap+ *scale-node*)
-    							 0.0)))))
-    ;; Attach node to ptr
-    (insert-node node *node-pointer* :out)
-    
-    ;; Advance pointer
-    (when move-pointer
-      (advance-node-of-node *node-pointer*
-    			    node
-    			    1.0))
-        
-    node))
-
 ;; rename to join
 (defun insert-node (node-src
 		    node-dest
@@ -299,8 +265,7 @@
 
 (defun pop-node (&key
 		   (node-ptr *node-pointer*)
-		   (dir-ptr :out)
-		   (update-transform t))
+		   (dir-ptr :out))
 
   ;; SHOULD only manage linkage, not moving
   
@@ -337,8 +302,7 @@
   ;;   - do nothing
 
   (when-let ((node-ref (get-node-ptr-out)))
-	    ;; Only unlink left side of pointer
-	    (unlink-node-ptr :out)
+	    (unlink-node-ptr :out) ; unlink ref only
 
 	    (multiple-value-bind (type-node-ref
 				  node-ref-in
@@ -349,52 +313,77 @@
 		     ;; Insert C out of A
 		     (insert-node node-ref-out node-ref-in :out)
 		     ;; Link pointer to next node
-		     (link-node-ptr node-ref-out)
-		     (when update-transform
-		       ;; Move C next to A
-  		       (advance-node-of-node node-ref-out
-  					     node-ref-in
-  					     1.0)	     
-		       ;; or use advance with self
-  		       (translate-node-to-node *node-pointer*
-  		       			       node-ref)))
+		     (link-node-ptr node-ref-out))
 		    
 		    ((eq type-node-ref :end)
-		     (link-node-ptr node-ref-in)
-		     (when update-transform
-		       ;; or use advance with self
-  		       (translate-node-to-node *node-pointer*
-  		       			       node-ref)))
+		     (link-node-ptr node-ref-in))
 
 		    ((eq type-node-ref :start)
-		     (link-node-ptr node-ref-out)
-		     (when update-transform
-		       ;; or use advance with self
-  		       (translate-node-to-node *node-pointer*
-  		       			       node-ref)))
+		     (link-node-ptr node-ref-out))
 
 		    ((eq type-node-ref :iso)
-		     (when update-transform
-		       ;; or use advance with self		     
-  		       (translate-node-to-node *node-pointer*
-  		       			       node-ref))))
-	      	      
+		     t))
+	      
 	      ;; Return deleted node, and surrounding nodes (for shm update)
 	      (values node-ref
+		      type-node-ref
 		      node-ref-in
 		      node-ref-out))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun add-node (data &optional (move-pointer t)) ; rename add-node-to-ptr
+  (let* ((baseline (get-origin-from-node-pos *node-pointer*))
+	 (node (init-node-msdf baseline
+			       *scale-node*
+			       (pop *stack-i-nodes*)
+			       data)))
+    
+    (insert-vertex node)
+    (spatial-trees:insert node *r-tree*)
+    
+    ;; Factor this out...
+    (when-let* ((node-ptr-out (get-node-ptr-out))
+		(node-type (get-node-type node-ptr-out)))
+
+	       ;; Push new node above for now
+	       (when (or (eq node-type :intra)
+			 (eq node-type :start))
+		 (let* ((bounds-origin (bounds-origin (gethash (char-code (data node)) *metrics*))))
+		   (translate-node-to-node node
+					   node
+					   :offset (vec3 0.0
+    							 (* +linegap+ *scale-node*)
+    							 0.0)))))
+    ;; Attach node to ptr
+    (insert-node node *node-pointer* :out)
+    
+    ;; Advance pointer
+    (when move-pointer
+      (advance-node-of-node *node-pointer*
+    			    node
+    			    1.0))
+        
+    node))
 
 (defun delete-node (&key
 		      (node-ptr *node-pointer*)
 		      (dir-ptr :out))
 
   (multiple-value-bind (node-ref
+			type-node-ref
 			node-ref-in
 			node-ref-out)
       (pop-node :node-ptr node-ptr
 		:dir-ptr dir-ptr)
     (when node-ref
-      ;; Remove from graph (removes edges?)
+      (translate-node-to-node *node-pointer*
+  		       	      node-ref)
+      (when (eq type-node-ref :intra)
+	;; Move C next to A
+  	(advance-node-of-node node-ref-out
+  			      node-ref-in
+  			      1.0))
       (push (index node-ref) *stack-i-nodes*)
       (remove-vertex node-ref)
       (spatial-trees:delete node-ref *r-tree*))
