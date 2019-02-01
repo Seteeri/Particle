0e6d2dceb41ece375c5d260e11880571a6574138
Kevin Ednalino <kcednalino@gmail.com>
2019-02-01 18:28:17 +0000

    Moved pointer operators from module node to operators

--- src/model/operators/node/operators-node.lisp
+++ src/model/operators/node/operators-node.lisp
@@ -162,6 +162,88 @@
   ;; Procedure:
   t)
 
+(defun move-node-ptr (dir) ; pass seq-event?
+  
+  ;; left = in
+  ;; right = out
+
+  (let* ((node-ref (get-node-ptr-out)))
+    
+    (if (not node-ref)
+	;; euclidean move
+	(progn
+	  (with-slots (model-matrix)
+	      *node-pointer*
+	    (translate-node-ptr nil
+				(lambda (value-new) ; update fn
+				  (setf (vx3 (translation model-matrix)) value-new)
+				  (enqueue-node-ptr))
+				(vx3 (translation model-matrix)) ; start
+				(* 96
+				   *scale-node*
+				   (cond ((eq dir :in)
+					  -1.0)
+					 ((eq dir :out)
+					  1.0)))
+				'move-pointer-x))
+	  (enqueue-node-ptr))
+
+	(when-let ((node-nxt (get-node-dir node-ref dir)))
+
+		  (unlink-node-ptr)
+		  (link-node-ptr node-nxt)
+		  
+		  ;; move ptr
+  		  ;; (advance-node-of-node *node-pointer*
+  		  ;; 			   *node-pointer*
+  		  ;; 			   (cond ((eq dir :in)
+		  ;; 				  -1.0)
+		  ;; 				 ((eq dir :out)
+		  ;; 				  1.0)))
+
+		  ;; move ref node right
+  		  ;; (advance-node-of-node node-ref
+  		  ;; 			   node-ref
+  		  ;; 			   (cond ((eq dir :in)
+		  ;; 				  1.0)
+		  ;; 				 ((eq dir :out)
+		  ;; 				  -1.0)))
+		  
+		  (translate-node-to-node *node-pointer*
+	     				  node-nxt)
+
+		  (fmt-model t "move-node-ptr" "* -> ~S = ~S~%" node-nxt (data node-nxt))
+		  
+		  ;; (enqueue-node node-ref)
+		  (enqueue-node-ptr)))))
+
+(defun translate-node-ptr (seq-event
+			   fn-new
+			   start
+			   delta
+			   id)
+  (when nil
+    (fmt-model t "translate-node-ptr" "~a, ~a -> ~a~%"
+	       id
+	       start
+	       (+ start delta)))
+  
+  (let ((anim (make-instance 'animation
+			     :id id
+			     :fn-easing #'easing:in-exp ;cubic
+			     :fn-new fn-new
+			     :value-start start
+			     :value-delta delta)))
+    ;; in animation.lisp
+    (enqueue-anim anim
+		  id
+		  (lambda ()
+		    (funcall #'run-anim
+			     seq-event
+			     anim)))))
+
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; (format t "*:: ~S~%" (digraph:predecessors *digraph* *node-pointer*))
