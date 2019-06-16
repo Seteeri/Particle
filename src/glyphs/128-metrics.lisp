7398eda7d4e9f06bfceedb8f70ea09ff2de7f46a
Kevin Ednalino <kcednalino@gmail.com>
2019-06-16 23:36:19 +0400

    Add dag.l, metrics.l; convert/refactor glyphs

--- src/glyphs/128-metrics.lisp
+++
@@ -1,5 +0,0 @@
-(list :code 128
-       :advance 9.375
-       :translate #(7.741848 7.741848)
-       :range 0.34341034
-       :bounds NIL)
--- src/glyphs/17-metrics.lisp
+++
@@ -1,5 +0,0 @@
-(list :code 17
-       :advance 9.375
-       :translate #(7.741848 7.741848)
-       :range 0.34341034
-       :bounds NIL)
