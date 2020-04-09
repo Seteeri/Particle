19cfc945ba37fe6ea9cde9ff3131bbd131d16bf8
Kevin Ednalino <kcednalino@gmail.com>
2020-04-09 21:45:16 +0100

    Improve shader

---
+++ glsl/basic.fs.glsl
@@ -0,0 +1,38 @@
+//#version 320 es
+
+precision mediump float;
+precision mediump samplerBuffer;
+
+uniform samplerBuffer msdf;
+
+in rgba_t vertexRGBA;
+flat in int vertexOffsetTex;
+flat in ivec2 vertexDimsTex;
+flat in vec2 vertexDimsTexOffset;
+in uv_t vertexUV;
+
+layout(location = 0) out vec4 color;
+
+float median(float r, float g, float b) 
+{
+    return max(min(r, g), min(max(r, g), b));
+}
+
+void main()
+{
+    vec4 samp = filter_bilinear(msdf, 
+                                vertexOffsetTex,
+                                vertexDimsTex,
+                                vertexUV,
+                                vertexDimsTexOffset);
+    float sigDist = median(samp.r, samp.g, samp.b);
+    float w = fwidth(sigDist);
+    float opacity = smoothstep(0.5 - w, 0.5 + w, sigDist);
+    
+    color = vec4(vec3(vertexRGBA.r,
+                       vertexRGBA.g,
+                       vertexRGBA.b),
+                 opacity*vertexRGBA.a);
+}
+
+// http://www.java-gaming.org/topics/solved-signed-distance-field-fonts-look-crappy-at-small-pt-sizes/33612/view.html
--- glsl/msdf.fs.glsl
+++ glsl/msdf.fs.glsl
@@ -18,21 +18,44 @@ float median(float r, float g, float b)
     return max(min(r, g), min(max(r, g), b));
 }
 
-void main()
+vec2 safeNormalize( in vec2 v )
 {
+   float len = length( v );
+   len = ( len > 0.0 ) ? 1.0 / len : 0.0;
+   return v * len;
+}
+
+void main(void)
+{
+    vec2 TexCoord = vec2(vertexUV.u, vertexUV.v) * vertexDimsTexOffset;
+    ivec2 iTexCoord = ivec2(TexCoord);
+    // Convert normalized texcoords to absolute texcoords.
+    vec2 uv = TexCoord * vec2(vertexDimsTex);
+    // Calculate derivates
+    vec2 Jdx = dFdx( uv );
+    vec2 Jdy = dFdy( uv );
+    // Sample SDF texture (3 channels).
     vec4 samp = filter_bilinear(msdf, 
                                 vertexOffsetTex,
                                 vertexDimsTex,
                                 vertexUV,
                                 vertexDimsTexOffset);
-    float sigDist = median(samp.r, samp.g, samp.b);
-    float w = fwidth(sigDist);
-    float opacity = smoothstep(0.5 - w, 0.5 + w, sigDist);
-    
-    color = vec4(vec3(vertexRGBA.r,
-                       vertexRGBA.g,
-                       vertexRGBA.b),
-                 opacity*vertexRGBA.a);
+    // calculate signed distance (in texels).
+    float sigDist = median( samp.r, samp.g, samp.b ) - 0.5;
+    // For proper anti-aliasing, we need to calculate signed distance in pixels. We do this using derivatives.
+    vec2 gradDist = safeNormalize( vec2( dFdx( sigDist ), dFdy( sigDist ) ) );
+    vec2 grad = vec2( gradDist.x * Jdx.x + gradDist.y * Jdy.x, gradDist.x * Jdx.y + gradDist.y * Jdy.y );
+    // Apply anti-aliasing = 1.0/8.0
+    //const float kThickness = 0.125;
+    const float kThickness = 1.0/8.0;
+    const float kNormalization = kThickness * 0.5 * sqrt( 2.0 );
+    float afwidth = min( kNormalization * length( grad ), 0.5 );
+    float opacity = smoothstep( 0.0 - afwidth, 0.0 + afwidth, sigDist );
+    // Apply pre-multiplied alpha with gamma correction.
+    vec4 uFgColor = vec4(vertexRGBA.r,
+                         vertexRGBA.g,
+                         vertexRGBA.b,
+                         vertexRGBA.a);
+    color.a = pow( uFgColor.a * opacity, 1.0 / 2.2 );
+    color.rgb = uFgColor.rgb * color.a;
 }
-
-// http://www.java-gaming.org/topics/solved-signed-distance-field-fonts-look-crappy-at-small-pt-sizes/33612/view.html
