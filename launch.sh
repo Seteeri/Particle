5e29d2f93e8430999780b4e1f0c7935ce6058483
Kevin Ednalino <kcednalino@gmail.com>
2020-04-29 14:16:35 +0100

    Launch processes from bash script

--- launch.sh
+++ launch.sh
@@ -1,2 +1,6 @@
 sudo pkill -f pil
-pil src/launch.l -main +
+
+pil -"scl 6" src/render/render.l -main + &
+pil -"scl 6" src/input/input.l   -main + &
+pil -"scl 6" src/ctrl/ctrl.l     -main + &
+pil -"scl 6" src/worker/worker.l -main + &
