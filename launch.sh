925450f2514ae1d8f1a1bf64ecc4fa20d476d8e9
Kevin Ednalino <kcednalino@gmail.com>
2020-05-25 15:34:13 +0100

    Rename src -> particle; move ops to super

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 sudo pkill -f pil
 
-pil -"scl 6" src/render/render.l -main + &
-pil -"scl 6" src/ctrl/ctrl.l     -main + &
-pil -"scl 6" src/input/input.l   -main + &
-pil -"scl 6" src/worker/worker.l -main + &
+pil -"scl 6" particle/render/render.l -main + &
+pil -"scl 6" particle/ctrl/ctrl.l     -main + &
+pil -"scl 6" particle/input/input.l   -main + &
+pil -"scl 6" particle/worker/worker.l -main + &
